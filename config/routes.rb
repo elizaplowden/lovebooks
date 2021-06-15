efb30b0f7858e2b2397c2be5ea1bc2d2d3b97277
Eliza Plowden <elizaplowden@gmail.com>
2021-06-15 14:12:46 +0100

    lists controller and actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'lists/show'
+  get 'lists/new'
+  get 'lists/create'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
