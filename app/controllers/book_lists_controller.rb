a621418dad1c9b9a509cac20c417ea3fd78ed64b
David Egerton <david.egertonsmith@gmail.com>
2021-06-15 16:33:07 +0100

    basic join table action and positioning of card

--- app/controllers/book_lists_controller.rb
+++ app/controllers/book_lists_controller.rb
@@ -1,7 +1,19 @@
-class ListBooksController < ApplicationController
+class BookListsController < ApplicationController
   def create
+    @BookList = BookList.new(booklist_params)
+    if @BookList.save
+      redirect_to list_path(current_user.lists.first)
+    else
+      redirect_to root_path
+    end
   end
 
   def delete
+  end
+
+  private
+
+  def booklist_params
+    params.require(:book_list).permit(:book_id, :list_id)
   end
 end
