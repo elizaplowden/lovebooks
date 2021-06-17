b6949e27a005ccc9130d23269d210960ad61a7b5
David Egerton <david.egertonsmith@gmail.com>
2021-06-17 17:44:52 +0100

    animations to accept and style on card

--- app/javascript/components/not_interested.js
+++ app/javascript/components/not_interested.js
@@ -1,13 +1,16 @@
 const notInterested = () => {
 
- const listTing = document.querySelectorAll(".buttons-holder");
+ const listTing = document.querySelectorAll(".btn-danger");
+ if(listTing) {
    listTing.forEach(item => {
     item.addEventListener('click', e => {
       const blarg = document.getElementById(`book_${e.target.id}`)
-      blarg.classList.add("rejected")
-      setTimeout(blarg.remove(), 3000)
+      blarg.classList.add("rejected");
+      const destroyer = () => { blarg.remove() };
+      let blob = setTimeout(destroyer , 3000);
     });
    });
+  }
 };
 
 export { notInterested };
